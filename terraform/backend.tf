4deca8189b584e9806c1c454f6956f95f8768838
diana_mih <diana.snd04@yahoo.ro>
2025-11-26 16:56:51 +0200

    Store state in S3

---
+++ terraform/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket         	   = "photorec-tfstate"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "eu-west-1"
+    encrypt        	   = true
+    dynamodb_table = "photo_rec_tf_lockid"
+  }
+}
